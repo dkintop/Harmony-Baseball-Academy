9acb77144fcaeb2eedde1a0733de9724dad1fc7c
Doug Kintop <dkintop@hotmail.com>
2020-05-18 16:28:04 -0400

    adds resized and edited Logo to src/images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
 import { FaRProject } from "react-icons/fa"
+import styles from "../css/navbar.module.css"
+import { FaAlignRight } from "react-icons/fa"
+import links from "../constants/navLinks"
+import socialIcons from "../constants/social-icons"
+import logo from "../images/Main-Logo.png"
 export default class Navbar extends Component {
+  state = {
+    isOpen: false,
+  }
+
   render() {
-    return <h1>I'm a Navbar</h1>
+    return (
+      <nav className={styles.navbar}>
+        <div className={styles.navCenter}>
+          <div className={styles.navHeader}>
+            <img
+              className={styles.mainLogo}
+              src={logo}
+              alt="Harmony Baseball Logo"
+            />
+          </div>
+        </div>
+      </nav>
+    )
   }
 }
