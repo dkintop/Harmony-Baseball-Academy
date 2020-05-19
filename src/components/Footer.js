d176769ea53e69b8d749e60d00defd6c053e31b1
Doug Kintop <dkintop@hotmail.com>
2020-05-19 13:40:40 -0400

    builds out footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react"
-
+import styles from "../css/footer.module.css"
+import links from "../constants/navlinks"
+import socialIcons from "../constants/social-icons"
+import { Link } from "gatsby"
+import { FaRproject } from "react-icons"
 export default class Footer extends Component {
   render() {
-    return <h1>I'm a footer</h1>
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.links}>
+          {links.map((item, index) => {
+            return (
+              <Link key={index} to={item.path}>
+                {item.text}
+              </Link>
+            )
+          })}
+        </div>
+        <div className={styles.icons}>
+          {socialIcons.map((item, index) => {
+            return (
+              <a key={index} href={item.url} target="_blank">
+                {item.icon}
+              </a>
+            )
+          })}
+        </div>
+      </footer>
+    )
   }
 }
