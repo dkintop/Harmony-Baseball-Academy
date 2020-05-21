7135dec340b7924febc5ccff2ead2ee642d931ad
Doug Kintop <dkintop@hotmail.com>
2020-05-21 11:45:21 -0400

    successfully reverts repository back to state that allows production build to successfully compile and adds css to blog page.

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,43 +1,30 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Link, graphql, StaticQuery } from "gatsby"
-
+import styles from "../css/blogRoll.module.css"
 class BlogRoll extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline">
+      <div className={styles.parent}>
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-6" key={post.id}>
-              <article
-                className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? "is-featured" : ""
-                }`}
-              >
-                <header>
-                  <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to={post.frontmatter.path}
-                    >
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <span className="subtitle is-size-5 is-block">
-                      {post.frontmatter.date}
-                    </span>
+            <div className={styles.postContainer} key={post.id}>
+              <article className={styles.blogListItem}>
+                <header className={styles.postHeader}>
+                  <p className={styles.postTitle}>
+                    {post.frontmatter.title}
+
+                    <span className={styles.bull}> &bull; </span>
+                    <span className={styles.date}>{post.frontmatter.date}</span>
                   </p>
                 </header>
-                <p>
+                <p className={styles.postBody}>
                   {post.excerpt}
                   <br />
                   <br />
-                  <Link className="button" to={post.frontmatter.path}>
-                    Keep Reading →
-                  </Link>
                 </p>
               </article>
             </div>
