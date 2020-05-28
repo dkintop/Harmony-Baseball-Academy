22ca717d904f9ee906c0a77dc4af1c86ec46ffdb
Doug Kintop <dkintop@hotmail.com>
2020-05-28 16:13:31 -0400

    modifies tyling in bio.module.css for CoachBioCards component

--- src/components/CoachBioCard.js
+++ src/components/CoachBioCard.js
@@ -2,22 +2,19 @@ import React, { Component } from "react"
 import styles from "../css/bio.module.css"
 import { Carousel } from "react-responsive-carousel"
 import { Card } from "react-bootstrap/Card"
+
 export default class CoachBioCard extends Component {
   render() {
     return (
-      <div className={styles.bioCard}>
-        <Carousel
-          className={styles.carousel}
-          showThumbs={false}
-          infiniteLoop={true}
-          interval={10000}
-          showIndicators={false}
-          showStatus={false}
-        >
-          <img className={styles.imageContainer} src={this.props.coach.image} />
-        </Carousel>
-
-        <div className={styles.bioContent}>{this.props.coach.bio}</div>
+      <div className={styles.card}>
+        <img
+          className={styles.cardImg}
+          src={this.props.coach.image}
+          alt={this.props.coach.name}
+        />
+        <h1>{this.props.coach.name}</h1>
+        <h3>{this.props.coach.roll}</h3>
+        <p> {this.props.coach.bio}</p>
       </div>
     )
   }
--- src/pages/bio.js
+++ src/pages/bio.js
@@ -3,20 +3,37 @@ import Layout from "../components/Layout"
 import coaches from "../constants/coaches"
 import styles from "../css/bio.module.css"
 import CoachBioCard from "../components/CoachBioCard.js"
-export default class Bio extends Component {
-  render() {
-    return (
-      <Layout>
-        <div className={styles.bioContainer}>
-          {coaches.map((coach, index) => {
-            return (
-              <>
-                <CoachBioCard coach={coach} />
-              </>
-            )
-          })}
-        </div>
-      </Layout>
-    )
-  }
+
+// export default class Bio extends Component {
+//   render() {
+//     return (
+//       <Layout>
+//         <div className={styles.bioContainer}>
+//           {coaches.map((coach, index) => {
+//             return (
+//               <>
+//                 <CoachBioCard coach={coach} />
+//               </>
+//             )
+//           })}
+//         </div>
+//       </Layout>
+//     )
+//   }
+// }
+
+export default function bio() {
+  return (
+    <Layout>
+      <div className={styles.bioContainer}>
+        {coaches.map((coach, index) => {
+          return (
+            <>
+              <CoachBioCard coach={coach} />
+            </>
+          )
+        })}
+      </div>
+    </Layout>
+  )
 }
