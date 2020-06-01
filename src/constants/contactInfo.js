55b514c92da68ba48dd9697b964f537ad3ef64b8
Doug Kintop <dkintop@hotmail.com>
2020-06-01 11:46:13 -0400

    adds content to contact page and builds layout for contact page.

---
+++ src/constants/contactInfo.js
@@ -0,0 +1,7 @@
+export default {
+  arsel: {
+    email: "arselgiowou4@gmail.com",
+    linkedin: "https://www.linkedin.com/in/arsel-giowou-b11369175/",
+    phone: "237690034094",
+  },
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react"
 import Layout from "../components/Layout"
+import styles from "../css/contact.module.css"
+import image from "../images/smile-throw.png"
+import otherImage from "../images/arsel-thumbsup.png"
+import contactInfo from "../constants/contactInfo.js"
 export default class Contact extends Component {
   render() {
     return (
       <Layout>
-        <div>Contact page</div>
+        <div className={styles.card}>
+          <img src={image} className={styles.image} />
+
+          <div className={styles.content}>
+            <h3>
+              For inquiries on how to get involved, please contact us, we would
+              love to hear from you!
+            </h3>
+            <h4>Arsel Giowou</h4>
+            <p>Email: {contactInfo.arsel.email}</p>
+            <p>
+              LinkedIn:
+              <a href={contactInfo.arsel.linkedin}>
+                {contactInfo.arsel.linkedin}
+              </a>
+            </p>
+            <p>Phone: {contactInfo.arsel.phone}</p>
+          </div>
+          <img src={otherImage} className={styles.image}></img>
+        </div>
       </Layout>
     )
   }
