ccb052a4fe96712fda133cebf62145b8d502ff0f
Doug Kintop <dkintop@hotmail.com>
2020-06-02 15:08:31 -0400

    adds social medial links to Arsel's CoachBioCard

--- src/components/CoachBioCard.js
+++ src/components/CoachBioCard.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react"
 import styles from "../css/bio.module.css"
-
+import socialIcons from "../constants/social-icons.js"
 export default class CoachBioCard extends Component {
+  conditionalLinks = coach => {
+    if (coach.name === "Arsel Giowou") {
+      return socialIcons.map((item, index) => {
+        return (
+          <a
+            style={{ margin: "5px" }}
+            key={index}
+            href={item.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {item.icon}
+          </a>
+        )
+      })
+    }
+  }
+
   render() {
     return (
       <div className={styles.card}>
@@ -12,6 +30,7 @@ export default class CoachBioCard extends Component {
         />
         <h1>{this.props.coach.name}</h1>
         <h3>{this.props.coach.role}</h3>
+        {this.conditionalLinks(this.props.coach)}
         <p> {this.props.coach.bio}</p>
       </div>
     )
