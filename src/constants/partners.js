f91c344b1264b01856e554f34783719ea8fbb08d
Doug Kintop <dkintop@hotmail.com>
2020-06-03 18:30:57 -0400

    corrects Bill Fanciscus role and adds to bio as well as corrects css color on hover for social links in footer.

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -4,6 +4,21 @@ import bill from "../images/bill.png"
 import { FaLinkedin } from "react-icons/fa"
 export default [
   {
+    name: "Bill Franciscus",
+    role: "CEO, Mentor",
+    image: bill,
+    bio:
+      "Global Ambassador for the African Youth Development Foundation, Founder and President of BASEBALL 4 LIFE non profit organization, Director of Baseball Operations and GM for USA Military WARDOGSBaseball.org, Director of Baseball Operations and Player Development for WE#21 www.we21.org, Pres. USA All-Star Baseball Association, Baseball Without Borders and Global Instructional Baseball League, Commissioner of the Cuban Sports League.",
+    quote: "If you're not giving, you're not living",
+    contact: {
+      email: "billychrist21@gmail.com",
+      linkedin: {
+        url: "https://www.linkedin.com/in/bill-franciscus-9b5b3141/",
+        icon: <FaLinkedin />,
+      },
+    },
+  },
+  {
     name: "Doug Kintop",
     role: "Web development and maintenance",
     image: doug,
@@ -15,21 +30,6 @@ export default [
       email: "dkintop@hotmail.com",
       linkedin: {
         url: "https://www.linkedin.com/in/doug-kintop/",
-        icon: <FaLinkedin />,
-      },
-    },
-  },
-  {
-    name: "Bill Franciscus",
-    role: "Mentor",
-    image: bill,
-    bio:
-      "Global Ambassador for the African Youth Development Foundation, Director of Baseball Operations and GM for USA Military WARDOGSBaseball.org, Director of Baseball Operations and Player Development for WE#21 www.we21.org, Pres. USA All-Star Baseball Association, Baseball Without Borders and Global Instructional Baseball League, Commissioner of the Cuban Sports League.",
-    quote: "If you're not giving, you're not living",
-    contact: {
-      email: "billychrist21@gmail.com",
-      linkedin: {
-        url: "https://www.linkedin.com/in/bill-franciscus-9b5b3141/",
         icon: <FaLinkedin />,
       },
     },
