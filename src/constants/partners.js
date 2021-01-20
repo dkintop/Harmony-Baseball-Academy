872daf34bf35cd928206b343488718302c67fcb9
Doug Kintop <dkintop@hotmail.com>
2021-01-19 21:07:54 -0500

    updates partners constant with Michael Baturin

--- src/constants/partners.js
+++ src/constants/partners.js
@@ -1,22 +1,24 @@
 import React from "react"
 import doug from "../images/doug.png"
 import bill from "../images/bill.png"
+import Michael from "../images/MichaelBaturin.jpg"
 import { FaLinkedin } from "react-icons/fa"
 export default [
   {
-    name: "Bill Franciscus",
-    role: "Mentor",
-    image: bill,
+    name: "Michael Baturin",
+    role: "US Outreach and Visibility Coordinator",
+    image: Michael,
     bio:
-      "Global Ambassador for the African Youth Development Foundation, Founder and President of BASEBALL 4 LIFE non profit organization, Director of Baseball Operations and GM for USA Military WARDOGSBaseball.org, Director of Baseball Operations and Player Development for WE#21 www.we21.org, Pres. USA All-Star Baseball Association, Baseball Without Borders and Global Instructional Baseball League, Commissioner of the Cuban Sports League.",
-    quote: "If you're not giving, you're not living",
+      "Michael Baturin is from Camp Hill, PA and connected immediately with Arsel and the Harmony Baseball Academy due to Arsel's deep enthusiasm for playing the game of baseball, an enthusiasm shared by Michael.  Michael's love for playing the game of baseball and a desire to spread it in its truest form to youths in Cameroon who are similarly enthusiastic about playing the game and who would benefit greatly from the opportunity to play it, is what drew Michael to help out the Harmony Baseball Academy",
+    quote:
+      "Working with Arsel has been inspirational.  He is an extraordinary person with a passion to bring baseball to Cameroon.  I love his energy and enthusiasm!" ,
     contact: {
-      email: "billychrist21@gmail.com",
+      email: "michaelmbaturin@gmail.com",
       linkedin: {
-        url: "https://www.linkedin.com/in/bill-franciscus-9b5b3141/",
-        icon: <FaLinkedin />,
-      },
-    },
+        url: "",
+        icon: " ",
+      }
+    }
   },
   {
     name: "Doug Kintop",
@@ -32,6 +34,23 @@ export default [
         url: "https://www.linkedin.com/in/doug-kintop/",
         icon: <FaLinkedin />,
       },
-    },
+      
+    }
   },
+  {
+    name: "Bill Franciscus",
+    role: "Mentor",
+    image: bill,
+    bio:
+      "Global Ambassador for the African Youth Development Foundation, Founder and President of BASEBALL 4 LIFE non profit organization, Director of Baseball Operations and GM for USA Military WARDOGSBaseball.org, Director of Baseball Operations and Player Development for WE#21 www.we21.org, Pres. USA All-Star Baseball Association, Baseball Without Borders and Global Instructional Baseball League, Commissioner of the Cuban Sports League.",
+    quote: "If you're not giving, you're not living",
+    contact: {
+      email: "billychrist21@gmail.com",
+      linkedin: {
+        url: "https://www.linkedin.com/in/bill-franciscus-9b5b3141/",
+        icon: <FaLinkedin />,
+      },
+    },
+  }
+  
 ]
